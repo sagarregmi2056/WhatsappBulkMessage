e05419fd926372b3b0dd21751e42dfa686ca62fa
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2024-12-04 16:18:18 +0545

    added app.spec.yaml

--- backend/server.js
+++ backend/server.js
@@ -362,7 +362,7 @@ app.post(
   }
 );
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8989;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
